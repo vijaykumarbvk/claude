878c0eb948eea14fd888eaa7992c32919147b0b5
VijayKumar <148474827+vijaykumarbvk@users.noreply.github.com>
2026-09-21 00:01:41 +0530

    Change S3 bucket name to 'venus-hospital-tfstate-vk'
    Updated S3 bucket name for Terraform backend.

--- EKS-Terraform/backend.tf
+++ EKS-Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "venus-hospital-tfstate-vijayk"   # <-- CHANGE to a globally-unique bucket
+    bucket       = "venus-hospital-tfstate-vk"   # <-- CHANGE to a globally-unique bucket
     key          = "venus-hospital/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
