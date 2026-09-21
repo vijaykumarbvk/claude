482f1b2ebbe7aba2f6d9c1fc71450020465363f7
VijayKumar <148474827+vijaykumarbvk@users.noreply.github.com>
2026-09-21 19:17:10 +0530

    Refactor database configuration in config.py

--- services/doctor_service/config.py
+++ services/doctor_service/config.py
@@ -1,12 +1,28 @@
+# import os
+
+
+# class Config:
+#     SQLALCHEMY_DATABASE_URI = os.environ.get(
+#         "DATABASE_URL",
+#         "mysql+pymysql://root:root@localhost:3306/venus_doctor_db",
+#     )
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     SERVICE_NAME = "doctor-service"
+#     SERVICE_PORT = int(os.environ.get("SERVICE_PORT", 8083))
+#     USER_SERVICE_URL = os.environ.get("USER_SERVICE_URL", "http://localhost:8081")
+
 import os
 
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL",
-        "mysql+pymysql://root:root@localhost:3306/venus_doctor_db",
+    SECRET_KEY = os.getenv('SECRET_KEY', 'super-secret-key')
+    
+    DB_HOST = os.getenv("DB_HOST", "mysql-service")
+    DB_PORT = os.getenv("DB_PORT", "3306")
+    DB_USER = os.getenv("DB_USER", "root")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+    DB_NAME = os.getenv("DB_NAME", "hospital_db")
+
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SERVICE_NAME = "doctor-service"
-    SERVICE_PORT = int(os.environ.get("SERVICE_PORT", 8083))
-    USER_SERVICE_URL = os.environ.get("USER_SERVICE_URL", "http://localhost:8081")
